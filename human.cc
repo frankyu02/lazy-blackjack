9c0116acbfcd07a8a855608e37c445280182e887
frankyu02 <frankyu02@yahoo.ca>
2022-09-29 00:51:39 -0400

    calculate score changed to human base class

---
+++ human.cc
@@ -0,0 +1,7 @@
+#include "human.h"
+#include "player.h"
+#include "deck.h"
+#include <iostream>
+#include <string>
+
+Human::Human(int m) : Player(m) {}
---
+++ human.h
@@ -0,0 +1,14 @@
+#ifndef HUMAN_H_
+#define HUMAN_H_
+#include "player.h"
+#include "deck.h"
+
+class Human : public Player
+{
+    Human(int m = 100);
+    void display_hand(std::vector<std::shared_ptr<Card>> hand) const override;
+    void play(std::shared_ptr<Deck> deck) override;
+    void hit(std::shared_ptr<Deck> deck, std::vector<std::shared_ptr<Card>> &hand) override;
+    void doubleDown(std::shared_ptr<Deck> deck, std::vector<std::shared_ptr<Card>> &hand) override;
+};
+#endif
